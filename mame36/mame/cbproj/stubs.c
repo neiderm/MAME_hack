0c0f4190a149ef81934db704c9271e507f870c13
gary7 <67ccmustang@gmail.com>
2015-07-29 14:00:43 -0400

    C:B project for Windows, Windows MultiMedia Extensions (WMME) and GDI (no DirectX)

--- mame36/mame/cbproj/stubs.c
+++ mame36/mame/cbproj/stubs.c
@@ -19,22 +19,25 @@ void vector_set_intensity(float _intensity)
 /*
  * from src/vidhrdw/avgdvg.c
  */
-int vector_updates; // referenced in  Throttle(void)
+int vector_updates; /* referenced in  Throttle(void)  */
 
 //#include <windef.h> // DWORD
 
 /*
- * These are needed to get around DirectX
+ * These are workarounds for cutting DirectX from Win32 osd and arguably 
+ * might belong in winmain.c
  */
-#include "options.h" // options_type
-#include "display.h" // OSDDisplay 
-struct OSDDisplay DDrawDisplay;
 
-#include "sound.h" // OSDSound
-struct OSDSound DirectSound;
+#include "options.h"  /* options_type */
+#include "display.h"  /* OSDDisplay */
+struct OSDDisplay DDrawDisplay;  /* from src/Win32/DDrawDisplay.c */ 
 
-#include "keyboard.h" // OSDKeyboard
-struct OSDKeyboard  DIKeyboard;
+#include "sound.h"  /* OSDSound */
+struct OSDSound DirectSound;  /* from src/Win32/DirectSound.c */ 
 
-#include "joystick.h" // OSDJoystick  
-struct OSDJoystick  DIJoystick;
+#include "keyboard.h"  /* OSDKeyboard */
+struct OSDKeyboard  DIKeyboard;  /* from src/Win32/DIKeyboard.c */
+
+#include "joystick.h"  /* OSDJoystick */
+struct OSDJoystick  DIJoystick;  /* from src/Win32/DIJoystick.c */
+
