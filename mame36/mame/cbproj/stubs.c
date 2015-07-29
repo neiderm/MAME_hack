a5347689947f4a7c8f1424b8ce0f4aa2ecbb47da
gary7 <67ccmustang@gmail.com>
2015-07-29 14:00:12 -0400

    C:B project for Windows, Windows MultiMedia Extensions (WMME) and GDI (no DirectX)

---
+++ mame36/mame/cbproj/stubs.c
@@ -0,0 +1,40 @@
+/*
+ * stubs for unresolved symbols
+ */ 
+
+
+/*
+ * vector stuff referenced in onscrd_vector_intensity()
+ * from src/vidhrdw/vector.c
+ */
+float vector_get_intensity(void)
+{
+  return 1.0;
+}
+
+void vector_set_intensity(float _intensity)
+{
+}
+
+/*
+ * from src/vidhrdw/avgdvg.c
+ */
+int vector_updates; // referenced in  Throttle(void)
+
+//#include <windef.h> // DWORD
+
+/*
+ * These are needed to get around DirectX
+ */
+#include "options.h" // options_type
+#include "display.h" // OSDDisplay 
+struct OSDDisplay DDrawDisplay;
+
+#include "sound.h" // OSDSound
+struct OSDSound DirectSound;
+
+#include "keyboard.h" // OSDKeyboard
+struct OSDKeyboard  DIKeyboard;
+
+#include "joystick.h" // OSDJoystick  
+struct OSDJoystick  DIJoystick;
