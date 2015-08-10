631c3c6cc88bc52f1d414a3a9b79f0597a29540b
gn <ded.reckning@gmail.com>
2015-08-10 08:02:07 -0400

    Definition of osd_cycles in header file must be inline

--- mame36/mame/src/Win32/osinline.h
+++ mame36/mame/src/Win32/osinline.h
@@ -2,7 +2,7 @@
 
     M.A.M.E.32  -  Multiple Arcade Machine Emulator for Win32
     Win32 Portions Copyright (C) 1997-98 Michael Soderstrom and Chris Kirmse
-    
+
     This file is part of MAME32, and may only be used, modified and
     distributed under the terms of the MAME license, in "readme.txt".
     By continuing to use, modify or distribute this file you indicate
@@ -75,7 +75,7 @@ INLINE int _vec_mult(int x, int y)
 
 #include "uclock.h"
 
-unsigned int osd_cycles(void)
+INLINE unsigned int osd_cycles(void)
 {
     return uclock();
 }
