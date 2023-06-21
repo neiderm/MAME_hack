c3130597d0e9042f9b5ded63797439e69a91b878
neiderm <ded.reckning@gmail.com>
2023-06-21 16:48:22 -0400

    Build script

--- mame0137s/build.sh
+++ mame0137s/build.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+# mame0137 on Xubuntu
 
-make -f makefile.unix  SHELL='sh -x'  MY_CPU=amd64  DISPLAY_METHOD=x11 SOUND_ALSA=1 LDFLAGS='' 
+# cat patch_xubuntu_14-04.txt  | patch -p1 --dry-run
 
+make AMD64=1 DEBUG=1 PTR64=1 NOWERROR=1
 
-# cat patch.txt  | patch -p1 --dry-run
+# mame0137s is presently only known to build in xubuntu 14.04 environment (more info needed)
+
+# VERSION="14.04.6 LTS, Trusty Tahr"
+
+# gcc -v:
+# Using built-in specs.
+# COLLECT_GCC=gcc
+# COLLECT_LTO_WRAPPER=/usr/lib/gcc/x86_64-linux-gnu/4.8/lto-wrapper
+# Target: x86_64-linux-gnu
+# Configured with: ../src/configure -v --with-pkgversion='Ubuntu 4.8.4-2ubuntu1~14.04.4' --with-bugurl=file:///usr/share/doc/gcc-4.8/README.Bugs --enable-languages=c,c++,java,go,d,fortran,objc,obj-c++ --prefix=/usr --program-suffix=-4.8 --enable-shared --enable-linker-build-id --libexecdir=/usr/lib --without-included-gettext --enable-threads=posix --with-gxx-include-dir=/usr/include/c++/4.8 --libdir=/usr/lib --enable-nls --with-sysroot=/ --enable-clocale=gnu --enable-libstdcxx-debug --enable-libstdcxx-time=yes --enable-gnu-unique-object --disable-libmudflap --enable-plugin --with-system-zlib --disable-browser-plugin --enable-java-awt=gtk --enable-gtk-cairo --with-java-home=/usr/lib/jvm/java-1.5.0-gcj-4.8-amd64/jre --enable-java-home --with-jvm-root-dir=/usr/lib/jvm/java-1.5.0-gcj-4.8-amd64 --with-jvm-jar-dir=/usr/lib/jvm-exports/java-1.5.0-gcj-4.8-amd64 --with-arch-directory=amd64 --with-ecj-jar=/usr/share/java/eclipse-ecj.jar --enable-objc-gc --enable-multiarch --disable-werror --with-arch-32=i686 --with-abi=m64 --with-multilib-list=m32,m64,mx32 --with-tune=generic --enable-checking=release --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu
+# Thread model: posix
+# gcc version 4.8.4 (Ubuntu 4.8.4-2ubuntu1~14.04.4) 
