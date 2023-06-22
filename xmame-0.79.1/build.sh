eeb9e61e6a6d71ff0b20ff5b5c844d05e03747d3
Glenn Neidermeier <neiderm@github.com>
2023-06-21 22:02:27 -0400

    Fix patch file

--- xmame-0.79.1/build.sh
+++ xmame-0.79.1/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+# cat patch.txt  | patch -p1 --dry-run
 
 make -f makefile.unix  SHELL='sh -x'  MY_CPU=amd64  DISPLAY_METHOD=x11 SOUND_ALSA=1 LDFLAGS='' 
 
 
-# cat patch.txt  | patch -p1 --dry-run
