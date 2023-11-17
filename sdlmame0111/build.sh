52f5c1e2a7481a5d622971da351ed1ad4275766f
Glenn Neidermeier <neiderm@gmail.com>
2023-11-17 15:20:30 -0500

    Update sdlmame0111 notes

--- sdlmame0111/build.sh
+++ sdlmame0111/build.sh
@@ -2,9 +2,20 @@
 
 #NAME="Ubuntu"
 #VERSION="20.04.6 LTS (Focal Fossa)"
+#VERSION="22.04.3 LTS (Jammy Jellyfish)"
+# install:
+# libgtk2.0-dev libgconf2-dev libsdl1.2-dev
+
+#  # patched makefile: 
+#  # PM=1
+#  AMD64=1 DEBUG=1 PTR64=1 NOWERROR=1
+#  # LDFLAGS = -WO
+#  LDFLAGS =
+#  LIBS = -lm -lpthread -lX11
+
+# schaser_sx10 redefined
 
 # cat xubuntu.pat  | patch -p1 --dry-run
 
-make -f makefile   NOWERROR=1
-
+make -f makefile  AMD64=1 DEBUG=1 PTR64=1 NOWERROR=1 
 
