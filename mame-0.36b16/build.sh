c593a9fc65dcf71350a4858daaf4b7e37d0912ef
Glenn Neidermeier <ded.reckning@gmail.com>
2023-11-21 06:35:19 -0500

    Update scripts, makefiles

--- mame-0.36b16/build.sh
+++ mame-0.36b16/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-#cat patch.txt | patch -p1 --dry-ru
-#make -f makefile.unix DISPLAY_METHOD=x11
+
+# todo: datafile, state added to OBJs 
+#  $(OBJ)/state.o $(OBJ)/datafile.o 
+# makeifle.unix:
+#  ESOUND_CFLAGS = -DSYSDEP_DSP_ALSA
+#  ESOUND_LIBS = -lasound
+
+#cat patch.txt | patch -p1 --dry-run
+
 make -f makefile.unix SHELL='sh -x'  MY_CPU=amd64  SOUND_ALSA=1 
+
