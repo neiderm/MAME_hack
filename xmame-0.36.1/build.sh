c6eed9090f277325a8cbc7125003c38dd820b5cd
Glenn Neidermeier <ded.reckning@gmail.com>
2023-11-22 08:23:35 -0500

    CodeBlocks project

--- xmame-0.36.1/build.sh
+++ xmame-0.36.1/build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-#cat patch.txt | patch -p1 --dry-ru
-make -f makefile.unix DISPLAY_METHOD=x11
+
+# todo:
+# CB project settings
+#  VERSION= 
+# makefile.unix:
+#  CFLAGS.amd64      = -DLSB_FIRST -DALIGN_INTS -DALIGN_SHORTS -D__LP64__
+
+#cat patch.txt | patch -p1 --dry-run
+
+make -f makefile.unix SHELL='sh -x'  MY_CPU=amd64  SOUND_ALSA=1 
+
