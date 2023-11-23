3f69903f7ae77819d29c5fcaaeae3fa7f6f844b4
Glenn Neidermeier <neiderm@gmail.com>
2023-11-23 16:24:46 -0500

    Update xmame-0.37b patches with ALSA sound

--- xmame-0.37b1.2/build.sh
+++ xmame-0.37b1.2/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-#todo: update patch for CFLAGS.amd64 in makefile
 
+# patched XMAME for ALSA sound on Linux, 
+# uses puckman ROM set
+# alsa.c from xmame-0.79.1, other dsp related files from xmame-0.37b7.1
 
-#cat patch.txt | patch -p1 --dry-ru
-#make -f makefile.unix DISPLAY_METHOD=x11
+# cat patch.txt | patch -p1 --dry-run
+
 make -f makefile.unix SHELL='sh -x'  MY_CPU=amd64  SOUND_ALSA=1 
