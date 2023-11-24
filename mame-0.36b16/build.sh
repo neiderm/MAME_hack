7bb6170852afd8b5814330d82533c1b224e8167d
Glenn Neidermeier <neiderm@github.com>
2023-11-24 10:57:01 -0500

    Update (cleanup) patch to install xmame-0.36.1 with backported ALSA sound. xmame-0.36.1 is upgrade with new ALSA interface backported from xmame-0.79.1, which also requires updating with sound streams interface from xmame-0.37

--- mame-0.36b16/build.sh
+++ mame-0.36b16/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# todo: datafile, state added to OBJs 
-#  $(OBJ)/state.o $(OBJ)/datafile.o 
-# makeifle.unix:
-#  ESOUND_CFLAGS = -DSYSDEP_DSP_ALSA
-#  ESOUND_LIBS = -lasound
+# Apply the patch to install xmame-0.36.1 with backported ALSA sound 
+# xmame-0.36.1 is modified for "new" ALSA interface (xmame-0.79.1) which also needs
+# upgrade to support streams which were added in xmame-0.37
 
-#cat patch.txt | patch -p1 --dry-run
+#cat xmame-0.36b16.patch | patch -p1 --dry-run
 
 make -f makefile.unix SHELL='sh -x'  MY_CPU=amd64  SOUND_ALSA=1 
 
